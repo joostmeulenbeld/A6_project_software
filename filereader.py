5e5cd2ae84dc4e4acaf23a9b25716c1b69762974
spcvanschie <sebastiaan.van.schie@live.nl>
2014-03-10 16:38:02 +0100

    boe

--- filereader.py
+++ filereader.py
@@ -3,5 +3,11 @@ import scipy
 import wave
 
 w=wave.open("Delfi-n3Xt.wav","r")
+
 params=w.getparams()
+nchannels=params[0]
+sampfreq=params[2]
+totalsamp=params[3]
 print params
+amp=w.readframes(1)
+print amp
