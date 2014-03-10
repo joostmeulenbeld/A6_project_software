d9a812f8795cc5108e2bbda7817a347da992b8a8
spcvanschie <sebastiaan.van.schie@live.nl>
2014-03-10 16:59:39 +0100

    Hai thar

--- filereader_test.py
+++ filereader_test.py
@@ -2,15 +2,16 @@ import scipy.io.wavfile as wav
 import scipy
 import wave
 
+fourierwidth= 100 ## Fourier transform interval width
 w=wave.open("Delfi-n3Xt.wav","r")
-
+nsample=0
 params=w.getparams()
 nchannels=params[0]
-sampfreq=params[2]
 sampwidth=params[1]
+sampfreq=params[2]
 totalsamp=params[3]
-print params
 
-for i in range(0,1000):
+## Divide sample file into intervals, one for every Fast Fourier Transform
+for i in range(nsample,nsample+fourierwidth):
     amp=w.readframes(i)
-    print amp
+    print i
