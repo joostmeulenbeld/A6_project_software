d5c93821981857a92eccaefb241143dd7616a7b1
ProfWolfs <rimskywolfs@hotmail.com>
2014-03-10 16:35:55 +0100

    highfrequency.py
    File for calculating the max frequency based on the matrix from group 1

---
+++ highfrequency.py
@@ -0,0 +1,20 @@
+# here comes some syntax to read the file from group 1
+
+
+# column per time interval with the frequencies versus the amplitude
+
+
+# while loop for all the time intervals
+# search for highest amplitude and give back the frequency
+# need some kind of algorithm to filter:
+#   noise, or peaks out of the boundary
+#   do we start in the middle because we know the carrier frequency
+#   and work outwards, so we can track it compared to result before?
+
+
+#print a file with for each time interval the highest frequency
+#i  | t(s)| f(Hz)
+#0  |  1  |  304
+#1  |  11 |  301
+#2  |  21 |  297
+#3  |  31 |  293
