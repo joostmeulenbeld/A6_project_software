a3ad9eed3abc68c4ebbe46c4e336854fe986f091
spcvanschie <sebastiaan.van.schie@live.nl>
2014-03-12 23:47:24 +0100

    End-of-session update

--- filereader.py
+++ filereader.py
@@ -1,16 +1,29 @@
 import scipy.io.wavfile as wav
 import scipy
 import wave
+import matplotlib.pyplot as plt
 
+fourierwidth= 100 ## Fourier transform interval width
 w=wave.open("Delfi-n3Xt.wav","r")
-
+nsample=0
+ltab=[]
+rtab=[]
+ttab=scipy.linspace(0,fourierwidth)
 params=w.getparams()
 nchannels=params[0]
-sampfreq=params[2]
 sampwidth=params[1]
+sampfreq=params[2]
 totalsamp=params[3]
-print params
 
-for i in range(0,1000):
+## Divide sample file into intervals, one for every Fast Fourier Transform
+for i in range(nsample,nsample+fourierwidth,2):
     amp=w.readframes(i)
-    print amp
+    ltab.append(amp)
+    rtab.append(i)
+
+## for i in range(nsample+1,nsample+fourierwidth,2):
+    
+    
+plt.plot(ltab,rtab)
+plt.title("Blablabla")
+plt.show
--- filereader_test.py
+++ filereader_test.py
@@ -1,17 +1,4 @@
 import scipy.io.wavfile as wav
 import scipy
 import wave
-
-fourierwidth= 100 ## Fourier transform interval width
-w=wave.open("Delfi-n3Xt.wav","r")
-nsample=0
-params=w.getparams()
-nchannels=params[0]
-sampwidth=params[1]
-sampfreq=params[2]
-totalsamp=params[3]
-
-## Divide sample file into intervals, one for every Fast Fourier Transform
-for i in range(nsample,nsample+fourierwidth):
-    amp=w.readframes(i)
-    print i
+import matplotlib.pyplot as plt
