afe538dafb8084e88ef84d7a8ea97866e9858a67
Joost Meulenbeld <joost.meulenbeld@gmail.com>
2014-03-13 14:17:45 +0100

    fouriertest updated works now!

--- fouriertranform_test.py
+++ fouriertranform_test.py
@@ -17,7 +17,9 @@ y = np.sin(50.0 * 2.0*np.pi*x) + 0.5*np.sin(80.0 * 2.0*np.pi*x)
 yf = frfft.getFFT(y)
 xf = np.linspace(0.0, 1.0/(2.0*T), N/2)
 
+print(xf)
+print(yf)
+
 plt.plot(xf, 2.0/N * np.abs(yf[0:N/2]))
 plt.show()
-print("Yolo")
-# print(yf)
+
