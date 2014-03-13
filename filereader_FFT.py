24ed5dc08ad70e8504f6136e1cc1b54888a9a04c
spcvanschie <sebastiaan.van.schie@live.nl>
2014-03-13 09:56:35 +0100

    Function statements
    Wrapper compatibility

--- filereader_FFT.py
+++ filereader_FFT.py
@@ -3,24 +3,24 @@ import scipy
 import wave
 import matplotlib.pyplot as plt
 
-fourierwidth= 100 ## Fourier transform interval width
-w=wave.open("Delfi-n3Xt.wav","r")
-nsample=0
-amptab=[]
-itab=[]
-ttab=scipy.linspace(0,fourierwidth)
-params=w.getparams()
-nchannels=params[0]
-sampwidth=params[1]
-sampfreq=params[2]
-totalsamp=params[3]
-print params
+def mainfft(w,fourierwidth):
+    fourierwidth= 100 ## Fourier transform interval width
+    nsample=0
+    amptab=[]
+    itab=[]
+    ttab=scipy.linspace(0,fourierwidth)
+    params=w.getparams()
+    nchannels=params[0]
+    sampwidth=params[1]
+    sampfreq=params[2]
+    totalsamp=params[3]
+    print params
 
 ## Divide sample file into intervals, one for every Fast Fourier Transform
-for i in range(nsample,nsample+fourierwidth):
-    amp=w.readframes(i)
-    amptab.append(amp)
-    itab.append(i)
-
-print amptab
-print itab
+def readout(nsample,fourierwidth):
+    for i in range(nsample,nsample+fourierwidth):
+        amp=w.readframes(i)
+        amptab.append(amp)
+        itab.append(i)
+    print amptab
+    print itab
