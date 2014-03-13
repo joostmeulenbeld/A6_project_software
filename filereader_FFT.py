49a36522dcea927724b7188eb836fc5d7f8dd977
spcvanschie <sebastiaan.van.schie@live.nl>
2014-03-13 10:19:02 +0100

    Intermediate changes

--- filereader_FFT.py
+++ filereader_FFT.py
@@ -4,20 +4,18 @@ import wave
 import matplotlib.pyplot as plt
 
 def mainfft(w,fourierwidth):
-    fourierwidth= 100 ## Fourier transform interval width
-    nsample=0
-    amptab=[]
-    itab=[]
     ttab=scipy.linspace(0,fourierwidth)
     params=w.getparams()
     nchannels=params[0]
     sampwidth=params[1]
     sampfreq=params[2]
     totalsamp=params[3]
-    print params
+    print params ## Channels, sample width [bytes], sample frequency
 
 ## Divide sample file into intervals, one for every Fast Fourier Transform
-def readout(nsample,fourierwidth):
+def readout(w,nsample,fourierwidth):
+    amptab=[]
+    itab=[]
     for i in range(nsample,nsample+fourierwidth):
         amp=w.readframes(i)
         amptab.append(amp)
--- filereader_test.py
+++ filereader_test.py
@@ -2,3 +2,7 @@ import scipy.io.wavfile as wav
 import scipy
 import wave
 import matplotlib.pyplot as plt
+import filereader_FFT as fft
+    
+a=fft.mainfft(wave.open('Delfi-n3Xt.wav','r'),10)
+b=fft.readout(wave.open('Delfi-n3Xt.wav','r'),0,10)
