09d50899744d8d7747ab6173acb9a752beb3b39b
Joost Meulenbeld <joost.meulenbeld@gmail.com>
2014-03-20 10:07:52 +0100

    Nothing happened

--- fouriertranform_test.py
+++ fouriertranform_test.py
@@ -1,6 +1,5 @@
 import numpy as np
 import filereader_FFT as frfft
-import pylab
 from matplotlib import pyplot as plt
 
 # t = np.arange(0, 2*np.pi, 0.1)
@@ -9,17 +8,15 @@ from matplotlib import pyplot as plt
 # print(timeamplitude)
 
 
-N = 600
-T = 1.0/800.0
+fs = 100
+T = 1.0/1000
+N = T*fs
 x = np.linspace(0.0, N*T, N)
-y = np.sin(50.0 * 2.0*np.pi*x) + 0.5*np.sin(80.0 * 2.0*np.pi*x)
+y = np.sin(50.0 * 2.0*np.pi*x) + 0.5*np.sin(80.0 * 2.0*np.pi*x) + np.sin(40.0* 2.0*np.pi*x)*1j
 
 yf = frfft.getFFT(y)
 xf = np.linspace(0.0, 1.0/(2.0*T), N/2)
 
-print(xf)
-print(yf)
 
 plt.plot(xf, 2.0/N * np.abs(yf[0:N/2]))
 plt.show()
-
