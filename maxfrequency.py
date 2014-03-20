b80d734c5cda8d98f0f4fba5071f397be243a39c
ProfWolfs <rimskywolfs@hotmail.com>
2014-03-20 11:32:10 +0100

    total change regarding meeting with bart

--- maxfrequency.py
+++ maxfrequency.py
@@ -1,30 +1,19 @@
 import numpy as np
 
-# here comes some syntax to read the file from group 1
-file=open("C:\Users\Rimsky\Documents\Canopy Enthought Python\Python codes\ja.txt","r")
-f=file.readlines()
+#find/make matrix A
+#find frequency vector f
+
+A=np.random.rand(4,4)
+B=np.zeros((4,2))
+for n in range (4):
+    h=max(A[n,:])
+    k=0
+    while A[n,k]<>h:
+        k=k+1
+    
+    B[n]=n,k
+    print h
+print A
+print B
 
 
-
-# column per time interval with the frequencies versus the amplitude
-g = np.genfromtxt(f,delimiter=" ")
-
-# while loop for all the time intervals
-# search for highest amplitude and give back the frequency
-# need some kind of algorithm to filter:
-#   noise, or peaks out of the boundary
-#   do we start in the middle because we know the carrier frequency
-#   and work outwards, so we can track it compared to result before?
-print str(g)
-h=max(g[:,1])
-n=0
-while g[n,1]!=h:
-    n=n+1
-print g[n,0]
-
-#print a file with for each time interval the highest frequency
-#i  | t(s)| f(Hz)
-#0  |  1  |  304
-#1  |  11 |  301
-#2  |  21 |  297
-#3  |  31 |  293
