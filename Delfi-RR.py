7443df72093c36a2da5d4b3a75bd477fd7acf8e1
Joost Meulenbeld <joost.meulenbeld@gmail.com>
2014-03-20 08:52:41 +0100

    Delfi-RR updated

--- Delfi-RR.py
+++ Delfi-RR.py
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 #import filereader_FFT
-=======
 import filereader_FFT as fft
->>>>>>> Fourier transform added, not working yet
 # import maxfrequency
 # import rangerate
 import tlerangerate
@@ -14,11 +11,8 @@ wav_filename = "Delfi-n3Xt.wav"
 fourierwidth = 100
 
 
-<<<<<<< HEAD
 #w = wave.open(wav_filename,"r")
 t = 20
 print tlerangerate.gs_plot(t)
-=======
 w = wave.open(wav_filename,"r")
 fouriermatrix = fft.mainfft(w, fourierwidth)
->>>>>>> Fourier transform added, not working yet
