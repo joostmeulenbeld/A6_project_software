c71e63b06dbd8a412a68948b726d8ced685b46bd
ProfWolfs <rimskywolfs@hotmail.com>
2014-03-20 11:35:09 +0100

    added some comment lines

--- maxfrequency.py
+++ maxfrequency.py
@@ -2,16 +2,21 @@ import numpy as np
 
 #find/make matrix A
 #find frequency vector f
-
 A=np.random.rand(4,4)
+#make zero matrix B for x,y values 
 B=np.zeros((4,2))
+
+#for loop for each row in matrix A
 for n in range (4):
+    #find max value in row A
     h=max(A[n,:])
     k=0
+    #find x,y value for the max in row n
     while A[n,k]<>h:
         k=k+1
-    
+    #print x,y value in matrix B
     B[n]=n,k
+    
     print h
 print A
 print B
