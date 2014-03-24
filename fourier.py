82d7d762b962d79ac13bdd9fe61e167349ff754c
Joost Meulenbeld <joost.meulenbeld@gmail.com>
2014-03-24 17:05:37 +0100

    fourier version updated

--- fourier.py
+++ fourier.py
@@ -1,4 +1,5 @@
 from scipy import fftpack
+from scipy import signal
 import numpy as np
 
 def getFFTs(T, timeamplitudes):
@@ -9,6 +10,20 @@ def getFFTs(T, timeamplitudes):
 def getFFT(T, timeamplitude):
     N = len(timeamplitude[0])
     fourieramplitudes = fftpack.fft(timeamplitude[1])
-    realamplitude = 2.0/N * np.abs(fourieramplitudes[0:N])
-    frequencies = np.fft.fftfreq(N)
-    return [frequencies, realamplitude]
+    if N % 2 == 0:
+    	positiveamplitude = 2.0/N * np.real(fourieramplitudes[0:N/2])
+    	negativeamplitude = 2.0/N * np.imag(fourieramplitudes[N/2:N])
+    else:
+    	positiveamplitude = 2.0/N * np.real(fourieramplitudes[0:(N+1)/2])
+    	negativeamplitude = 2.0/N * np.imag(fourieramplitudes[(N+1)/2:N])
+
+    positiveamplitude = positiveamplitude[::-1] #The negative frequencies have to be reversed
+    negativeamplitude = negativeamplitude[::-1]
+    amplitudes = np.append(positiveamplitude, negativeamplitude)
+
+    frequencies = fftpack.fftshift(np.fft.fftfreq(N, T))
+    return frequencies, amplitudes
+
+# def getFFT(x, fs):
+# 	frequencies, power = signal.periodogram(x, fs=fs)
+# 	return frequencies, power
