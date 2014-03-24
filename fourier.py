22576305d6b5a76162bdb74ebac28a6dcd9541dd
Joost Meulenbeld <joost.meulenbeld@gmail.com>
2014-03-24 14:48:03 +0100

    fft_backup

--- fourier.py
+++ fourier.py
@@ -1,4 +1,5 @@
-
+from scipy import fftpack
+import numpy as np
 
 def getFFTs(T, timeamplitudes):
     frequencyamplitudes = []
@@ -6,8 +7,8 @@ def getFFTs(T, timeamplitudes):
         frequencyamplitudes.append(getFFT(T, ta))
 
 def getFFT(T, timeamplitude):
-    N = len(timeamplitudes[0])
-    fourieramplitudes = fftpack.fft(timeamplitudes[1])
-    realamplitudes = 2.0/N * np.abs(fourieramplitudes[0:N/2])
-    frequencies = np.linspace(0.0, 1.0/(2.0*T), N/2)
-    return [frequencies, realamplitudes]
+    N = len(timeamplitude[0])
+    fourieramplitudes = fftpack.fft(timeamplitude[1])
+    realamplitude = 2.0/N * np.abs(fourieramplitudes[0:N])
+    frequencies = np.fft.fftfreq(N)
+    return [frequencies, realamplitude]
