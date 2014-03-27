a4b9f10a03773abb6dbeafeca176e297fb2cfb6a
ProfWolfs <rimskywolfs@hotmail.com>
2014-03-27 12:00:36 +0100

    added some more stuff and factors, made it usable with any size matrix

--- maxfrequency.py
+++ maxfrequency.py
@@ -6,24 +6,27 @@ import numpy as np
 
 #find/make matrix A
 #find frequency vector f
-
 A=np.array([[0.,0.,0.,0.,0.,0.,1.],
-            [0.,0.,1.,0.,0.,1.,0.],
+            [0.,1.,0.,0.,0.,1.,0.],
+            [0.,0.,0.,0.,1.,0.,0.],
+            [0.,0.,0.,1.,0.,0.,0.],
+            [0.,0.,1.,0.,0.,1.,1.],
             [0.,1.,0.,0.,1.,0.,0.],
-            [0.,0.,0.,1.,0.,1.,0.],
-            [0.,0.,1.,0.,0.,1.,0.],
-            [0.,1.,0.,0.,1.,0.,0.],
-            [1.,0.,0.,0.,0.,0.,0.]])
-#make zero matrix B for x,y values 
-B=np.zeros((7,2))
-columnsA=7
-
+            [1.,0.,0.,1.,0.,0.,1.]])
+#make zero matrix B for x,y values, same amount of rows as A 
+C=np.shape(A)
+rowsA=C[0]
+columnsA=C[1]
+B=np.zeros((rowsA,2))
+factorrange=0.75 #factor of how much of the columns of A range will be used
+factorr=0.25      #q=r-round(factorr*r) how much smaller q should be if its bigger than r
+factorq=0.1      #q=q+round(factorq*columnsA) how much smaller the interval gets
 #############################################################################################
         #First run of least square finding and rewrite matrix B with elast square coordinates
 #############################################################################################
 
 #for loop for each row in matrix A
-for n in range (7):
+for n in range (rowsA):
     #find max value in row A
     h=max(A[n,:])
     k=0
@@ -40,7 +43,7 @@ def func(x, a, b, c ,d):
 # Create the data for curve_fit.
 xdata = B[:,0]
 ydata = B[:,1]
-sigma = np.ones(columnsA)
+sigma = np.ones(rowsA)
 x0    = np.array([0.0, 0.0, 0.0, 0.0])
 import scipy.optimize as optimization
 
@@ -48,7 +51,7 @@ import scipy.optimize as optimization
 [popt, pcov] = optimization.curve_fit(func, xdata, ydata, x0, sigma)
 
 #look for closest points to the least square function
-for m in range(7):
+for m in range(rowsA):
     p=popt[0] + popt[1]*B[m,0] + popt[2]*B[m,0]*B[m,0] + popt[3]*B[m,0]*B[m,0]*B[m,0]
     pr=round(popt[0] + popt[1]*B[m,0] + popt[2]*B[m,0]*B[m,0] + popt[3]*B[m,0]*B[m,0]*B[m,0])  
     #replace values of B by closest values in the least square  
@@ -59,31 +62,33 @@ print B #for check, new matrix with least square values
 #############################################################################################
        #Same as above but iterate within a range next to the least square which gets smaller     
 #############################################################################################
-
+#define range r interval of 25% one ach side, q will make this smaller each time
+r=round(columnsA*factorrange)
+print r
 q=0
-for z in range (3):
+for z in range (5):
         #for loop for each row in matrix A
-    for n in range (7):
+    for n in range (rowsA):
         #find max value in row A CLOSE TO least square
         c=B[n,1]
         
-        d=int(c-2+q)
+        d=int(c-r+q)
         if d <= 0:
             d = 0
-        e=int(c+5-q)
+        e=int(c+(2*r)+(-q))
         if e <= 0:
             e = 0
-        if e >= 7:
-            e=7
+        if e >= columnsA:
+            e=columnsA
         k=0
         
-        print "-"
-        print n
-        print c
-        print d
-        print e
+     #   print "-"
+      #  print n
+       # print c
+        #print d
+        #print e
         h=max(A[n][d:e])
-        print h
+        #print h
         #find x,y value for the max in row n in A
         while A[n,d]<>h:
             d=d+1
@@ -100,7 +105,7 @@ for z in range (3):
     # Create the data for curve_fit.
     xdata = B[:,0]
     ydata = B[:,1]
-    sigma = np.ones((columnsA))
+    sigma = np.ones((rowsA))
     x0    = np.array([0.0, 0.0, 0.0, 0.0])
     import scipy.optimize as optimization
     
@@ -109,16 +114,27 @@ for z in range (3):
     
     
     #look for closest points to the least square function
-    for m in range(7):
+    for m in range(rowsA):
         p=popt[0] + popt[1]*B[m,0] + popt[2]*B[m,0]*B[m,0] + popt[3]*B[m,0]*B[m,0]*B[m,0]
         pr=round(popt[0] + popt[1]*B[m,0] + popt[2]*B[m,0]*B[m,0] + popt[3]*B[m,0]*B[m,0]*B[m,0])  
         #replace values of B by closest values in the least square  
         B[m]=[m,pr]
-    q=q+1
+    #if q gets larger than r(range) it gets maxed to r minus 10 percent
+    q=q+round(factorq*columnsA)
+    if q >= r:
+        q=r-round(factorr*r)
     print "next q is",q
+    print "the r is", r
     print B
 
 #############################################################################################
-        #Give coordinates of the maxes found ion the matrix close to its least squares in B
+        #Give coordinates of the maxes found on the matrix close to its least squares in B
 #############################################################################################
 print B 
+#print popt
+#for s in range(rowsA):
+   # B[s,1]=A[s,B[s,1]]
+#print A
+#print B
+
+
