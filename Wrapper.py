02d46ad0de8eb8266a7835c32ea583b678ababe7
Joost Meulenbeld <joost.meulenbeld@gmail.com>
2014-04-23 09:01:37 +0200

    Wrapper updated
    Now the other functions can be implemented here

--- Wrapper.py
+++ Wrapper.py
@@ -4,11 +4,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 from wavReadFourier import wavReaderFourierTransformer
 
+''' Input variables '''
+
 wavFileName = "Delfi-n3xt.wav"
 start = 11.0
 end = 13.0
 intervalWidth = 1.0
 intervalStartFrequency = 1.0
+
+''' End of input variable
+
+No code here
+No code here
+
+Wav reading and Fourier transforming'''
 
 wavReader = wavReaderFourierTransformer(wavFileName, start, end, intervalWidth, intervalStartFrequency)
 frequencies, intervals = wavReader.getFrequencyAmplitudes()
@@ -16,9 +25,49 @@ frequencies, intervals = wavReader.getFrequencyAmplitudes()
 print("Maximum Fourier frequency: " + str(wavReader.getMaxFourierFrequency()))
 print("Delta Fourier frequency: " + str(wavReader.getDeltaFourierFrequency()))
 
-
-
 for i in range(len(intervals)):
 	plt.plot(frequencies, intervals[i][1])
 
 plt.show()
+
+''' End of wav reading and fourier transforming
+
+No code here
+No code here
+
+Maximum frequency and outlier detection '''
+
+
+
+
+
+
+''' End of Maximum frequency and outlier detection
+
+No code here
+No code here
+
+Doppler tracking '''
+
+
+
+
+
+
+
+''' End of Doppler tracking 
+
+No code here
+No code here
+
+Post-processing'''
+
+
+
+
+''' End of Post-processing
+
+No code here
+No code here
+
+'''
