1effbb6c1fe3d60af62086c91898ab66cc91778f
MaartenSchild <maartenschild@hotmail.com>
2014-05-07 12:15:05 +0200

    wijmoetenbeslissen

--- maxfrequencyclean.py
+++ maxfrequencyclean.py
@@ -48,27 +48,58 @@ def maxFrequencies(wavReader, carrierfrequency):
     #############################################################################################
             #First run of least square finding and rewrite matrix B with least square coordinates
     #############################################################################################
-    lst=[]    
-    for j in range(len(A)):
+#    lst=[]    
+#    for j in range(len(A)):
+#        lst2=[]
+#        for i in range((len(A[j])-1000)/10):
+#            absLst = [h if h > 0 else -h for h in A[j][i*10:(i*10)+1000]] 
+#            lst2.append(sum(absLst))
+#        
+#        lst.append(lst2)
+#        print "Searching first estimate in interval: ",j+1,"/",len(A)
+#    maxfreqlist = []
+#    print "Found first estimate intervals"
+#    print "Searching for max frequencies in intervals"
+#    for g in range(len(lst)):
+#        print max(lst[g])
+#        maxintervalindex = lst[g].index(max(lst[g]))
+#        maxfreqindex = A[g][((maxintervalindex*10)):(maxintervalindex*10)+1000].argmax()
+#        maxfreqindex +=(maxintervalindex*10)
+#        maxfreqlist.append(frequencies[maxfreqindex])
+#        print "Interval: ",g+1,"/",len(lst)," Found Max Freq: ",maxfreqindex
+#    
+#    print "Found all maximum frequencies OLLD"
+    ##
+    lst = []
+    for interval in range(len(A)): 
         lst2=[]
-        for i in range((len(A[j])-1000)/10):
-            absLst = [h if h > 0 else -h for h in A[j][i*10:(i*10)+1000]] 
+        absLst = [h if h > 0 else -h for h in A[interval][0:1000]] 
+        lst2.append(sum(absLst))        
+        for i in range((len(A[interval])-1010)/10):
+            lst3 = [h if h > 0 else -h for h in A[interval][1000+(i*10):(i*10)+1010]]
+            absLst = absLst[10:1000]
+            absLst.extend(lst3)
             lst2.append(sum(absLst))
-        
         lst.append(lst2)
-        print "Searching first estimate in interval: ",j+1,"/",len(A)
+        print "Searching first estimate in interval: ",interval+1,"/",len(A)
+        
+        
     maxfreqlist = []
     print "Found first estimate intervals"
     print "Searching for max frequencies in intervals"
     for g in range(len(lst)):
+        print max(lst[g])
         maxintervalindex = lst[g].index(max(lst[g]))
         maxfreqindex = A[g][((maxintervalindex*10)):(maxintervalindex*10)+1000].argmax()
-        maxfreqindex +=(maxintervalindex*10)-1000
+        maxfreqindex +=(maxintervalindex*10)
         maxfreqlist.append(frequencies[maxfreqindex])
         print "Interval: ",g+1,"/",len(lst)," Found Max Freq: ",maxfreqindex
     
     print "Found all maximum frequencies"
-     
+            
+            
+            
+            
     #############################################################################################
             #Give coordinates of the maxes found on the matrix close to its least squares in B
     #############################################################################################
