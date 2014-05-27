943bbd4bcc5978474f7c843ef599b03c2f06d1ba
mmartinmendez <mithun.martin.215@gmail.com>
2014-05-27 14:11:54 +0200

    added again

---
+++ recursion3.py
@@ -0,0 +1,21 @@
+from scikits.audiolab import Sndfile
+from scikits.audiolab import wavread
+import numpy as np
+import matplotlib.pyplot as p
+
+
+def output_signal(no_of_frames,start,raw):
+    left_channel=np.zeros(no_of_frames)
+    right_channel=np.zeros(no_of_frames)
+    signal=[0 for i in range(no_of_frames)]
+    raw.seek(start)
+    data = np.array(raw.read_frames(no_of_frames), dtype=np.float64)
+    for y in range(0,no_of_frames):
+        left_channel[y]=data[y][0]
+        right_channel[y]=data[y][1]
+    left=np.array(left_channel)
+    right=np.array(right_channel)
+    signal=left+right*1j
+    return signal
+
+
